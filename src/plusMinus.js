7d7b07fdffa04a6e756f138d52856de9d1992e01
Icaro <luizicaro.s@gmail.com>
2023-01-01 12:52:43 -0300

    Adjusting eslint

--- src/plusMinus.js
+++ src/plusMinus.js
@@ -1,11 +1,11 @@
 /*
-* Given an array of integers, calculate the ratios of its elements that are positive,
-* negative, and zero.
-*
-* Print the decimal value of each fraction on a new line with  places after the decimal.
-*
-* The function accepts INTEGER_ARRAY arr as parameter.
-*/
+ * Given an array of integers, calculate the ratios of its elements that are positive,
+ * negative, and zero.
+ *
+ * Print the decimal value of each fraction on a new line with  places after the decimal.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
 
 export default function plusMinus(arr) {
   const positives = arr.filter((number) => number > 0).length;
