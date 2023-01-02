8012b765b385fe74c88a12a48d5f70873a0be947
Icaro <luizicaro.s@gmail.com>
2023-01-02 19:01:35 -0300

    Impementing processaData function

--- src/camelCase4.js
+++ src/camelCase4.js
@@ -15,6 +15,32 @@
  * Methods should end with an empty set of parentheses to differentiate them from variable names.
  */
 
-export default function processData(input) {
+function replacer(match, index) {
+  if (index > 0) return ` ${match}`;
+  return match;
+}
 
+export default function processData(input) {
+  const lines = input.split('\r\n');
+
+  lines.forEach((element) => {
+    const splittedLine = element.split(';');
+    const operation = splittedLine[0];
+    const method = splittedLine[1];
+    let data = splittedLine[2];
+    if (operation === 'S') {
+      const regex = /[A-Z]/gm;
+      data = data.replace(regex, replacer).toLowerCase();
+      data = data.replace('()', '');
+      console.log(data);
+    } else if (operation === 'C') {
+      data = data.split(' ');
+      data = data.map((word) => word[0].toUpperCase() + word.substring(1)).join('');
+      if (method !== 'C') {
+        data = data[0].toLowerCase() + data.substring(1);
+        if (method === 'M') data += '()';
+      }
+      console.log(data);
+    }
+  });
 }
