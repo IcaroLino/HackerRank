46cc48c374f51ad45bddb8864bf112d4b1d0ed64
Icaro <luizicaro.s@gmail.com>
2023-01-05 16:37:08 -0300

    Adding pangram test

---
+++ src/week2/pangrams.js
@@ -0,0 +1,12 @@
+/**
+ * A pangram is a string that contains every letter of the alphabet.
+ * Given a sentence determine whether it is a pangram in the English alphabet.
+ * Return either pangram or not pangram as appropriate
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+export default function pangrams(s) {
+
+}
--- test/week2.test.js
+++ test/week2.test.js
@@ -2,11 +2,28 @@ import {
   describe, expect, it,
 } from '@jest/globals';
 import countingValleys from '../src/week2/countingValleys';
+import pangrams from '../src/week2/pangrams';
 
 describe('countingValleys Test', () => {
   it('Must return the amount of valleys', () => {
     const returnedValue = countingValleys(12, 'DDUUDDUDUUUD');
     const expectedValue = 2;
+
+    expect(returnedValue).toBe(expectedValue);
+  });
+});
+
+describe('pangrams Test', () => {
+  it('Must return pangram', () => {
+    const returnedValue = pangrams('We promptly judged antique ivory buckles for the next prize');
+    const expectedValue = 'pangram';
+
+    expect(returnedValue).toBe(expectedValue);
+  });
+
+  it('Must return not pangram', () => {
+    const returnedValue = pangrams('We promptly judged antique ivory buckles for the prize');
+    const expectedValue = 'not pangram';
 
     expect(returnedValue).toBe(expectedValue);
   });
