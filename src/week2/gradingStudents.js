719ba87eefa6a2921ef9a35580bc684cf18cd600
Icaro <luizicaro.s@gmail.com>
2023-01-10 21:30:33 -0300

    Implementing gradingStudents function

--- src/week2/gradingStudents.js
+++ src/week2/gradingStudents.js
@@ -9,5 +9,8 @@
  */
 
 export default function gradingStudents(grades) {
-
+  return grades.map((grade) => {
+    if (grade < 38 || (grade % 5) < 3) return grade;
+    return grade + 5 - (grade % 5);
+  });
 }
