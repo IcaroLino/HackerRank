690372ee16600d8e9b301e717631143cbb73894d
Icaro <luizicaro.s@gmail.com>
2023-01-11 18:20:25 -0300

    Implementing countingSort Test

---
+++ src/week2/countingSort1.js
@@ -0,0 +1,10 @@
+/*
+ * Given a list of integers, count and return the number of times each value appears.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+export default function countingSort(arr) {
+
+}
