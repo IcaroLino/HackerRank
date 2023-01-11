a89e84f96549e64436491a84ce473a49cd9300d2
Icaro <luizicaro.s@gmail.com>
2023-01-11 18:22:01 -0300

    Implementing countingSort function

--- src/week2/countingSort1.js
+++ src/week2/countingSort1.js
@@ -6,5 +6,11 @@
  */
 
 export default function countingSort(arr) {
+  const frequencyArr = new Array(100).fill(0);
 
+  for (let index = 0; index < arr.length; index += 1) {
+    frequencyArr[arr[index]] += 1;
+  }
+
+  return frequencyArr;
 }
