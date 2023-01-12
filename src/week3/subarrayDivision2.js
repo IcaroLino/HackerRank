f4f4036ec51e0b388088c0952ba2945fa16571c2
Icaro <luizicaro.s@gmail.com>
2023-01-11 23:52:37 -0300

    Adding subarrayDivision 2 Test

---
+++ src/week3/subarrayDivision2.js
@@ -0,0 +1,17 @@
+/*
+ * Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it.
+ * Lily decides to share a contiguous segment of the bar selected such that:
+ *
+ * The length of the segment matches Ron's birth month, and,
+ * The sum of the integers on the squares is equal to his birth day.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+export default function birthday(s, d, m) {
+
+}
--- test/week3.test.js
+++ test/week3.test.js
@@ -2,6 +2,7 @@ import {
   describe, expect, it,
 } from '@jest/globals';
 import twoArrays from '../src/week3/permutingTwoArrays';
+import birthday from '../src/week3/subarrayDivision2';
 
 describe('permutingTwoArrays Test', () => {
   it('Must return YES', () => {
@@ -14,6 +15,15 @@ describe('permutingTwoArrays Test', () => {
   it('Must return NO', () => {
     const returnedValue = twoArrays(5, [1, 2, 2, 1], [3, 3, 3, 4]);
     const expectedValue = 'NO';
+
+    expect(returnedValue).toBe(expectedValue);
+  });
+});
+
+describe('subarrayDivision 2 Test', () => {
+  it('Must return the numbers of ways the bar can be divided', () => {
+    const returnedValue = birthday([1, 2, 1, 3, 2], 3, 2);
+    const expectedValue = 2;
 
     expect(returnedValue).toBe(expectedValue);
   });
