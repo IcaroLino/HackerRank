4973df15081b711a691859f98455f72561b992c6
Icaro <luizicaro.s@gmail.com>
2023-01-14 22:29:25 -0300

    Implementing maximumPerimeterTriangle function

--- src/week3/maximumPerimeterTriangle.js
+++ src/week3/maximumPerimeterTriangle.js
@@ -1,10 +1,19 @@
 /**
- *
+ * Given an array of stick lengths, use 3 of them to construct a non-degenerate triangle
+ * with the maximum possible perimeter.
+ * Return an array of the lengths of its sides as 3 integers in non-decreasing order.
  *
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts INTEGER_ARRAY sticks as parameter.
  */
 
 export default function maximumPerimeterTriangle(sticks) {
+  sticks.sort((a, b) => b - a);
 
+  for (let index = 0; index < sticks.length; index += 1) {
+    const triplet = sticks.slice(index, index + 3);
+    if (triplet[0] < triplet[1] + triplet[2]) return triplet.reverse();
+  }
+
+  return [-1];
 }
