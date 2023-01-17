2998d785555fe33c3e3a814d885f82dd37a3f3d9
Icaro <luizicaro.s@gmail.com>
2023-01-17 20:01:48 -0300

    Adding leftRotation Test

--- test/week4.test.js
+++ test/week4.test.js
@@ -1,12 +1,22 @@
 import {
   describe, expect, it,
 } from '@jest/globals';
+import rotateLeft from '../src/week4/leftRotation';
 import pickingNumbers from '../src/week4/pickingNumbers';
 
 describe('pickingNumbers Test', () => {
   it('Must return the length of the longest subarray that meets the criterion', () => {
     const returnedValue = pickingNumbers([4, 6, 5, 3, 3, 1]);
     const expectedValue = 3;
+
+    expect(returnedValue).toBe(expectedValue);
+  });
+});
+
+describe('leftRotation Test', () => {
+  it('Must return the rotated array', () => {
+    const returnedValue = rotateLeft(4, [1, 2, 3, 4, 5]);
+    const expectedValue = [5, 1, 2, 3, 4];
 
     expect(returnedValue).toBe(expectedValue);
   });
