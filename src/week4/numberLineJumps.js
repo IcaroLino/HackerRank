e45ab89f33714d9cf43833614f431694eb0205bf
Icaro <luizicaro.s@gmail.com>
2023-01-17 20:40:14 -0300

    Adding numberLineJumps Test

---
+++ src/week4/numberLineJumps.js
@@ -0,0 +1,20 @@
+/**
+ * The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump
+ * The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+ *
+ * You have to figure out a way to get both kangaroos
+ * at the same location at the same time as part of the show.
+ *
+ * If it is possible, return YES, otherwise return NO.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+export default function kangaroo(x1, v1, x2, v2) {
+
+}
--- test/week4.test.js
+++ test/week4.test.js
@@ -2,6 +2,7 @@ import {
   describe, expect, it,
 } from '@jest/globals';
 import rotateLeft from '../src/week4/leftRotation';
+import kangaroo from '../src/week4/numberLineJumps';
 import pickingNumbers from '../src/week4/pickingNumbers';
 
 describe('pickingNumbers Test', () => {
@@ -19,5 +20,21 @@ describe('leftRotation Test', () => {
     const expectedValue = [5, 1, 2, 3, 4];
 
     expect(returnedValue).toEqual(expectedValue);
+  });
+});
+
+describe('numberLineJumps Test', () => {
+  it('Must return YES', () => {
+    const returnedValue = kangaroo(0, 3, 4, 2);
+    const expectedValue = 'YES';
+
+    expect(returnedValue).toBe(expectedValue);
+  });
+
+  it('Must return NO', () => {
+    const returnedValue = rotateLeft(0, 2, 5, 3);
+    const expectedValue = 'NO';
+
+    expect(returnedValue).toBe(expectedValue);
   });
 });
