a352e4dfb8ec9025a982cc02350849982ec930e3
Icaro <luizicaro.s@gmail.com>
2023-01-31 20:55:14 -0300

    Implementing strongPassword test

---
+++ src/week5/strongPassword.js
@@ -0,0 +1,20 @@
+/*
+ * The website considers a password to be strong if it satisfies the following criteria:
+ *
+ * Its length is at least .
+ * It contains at least one digit.
+ * It contains at least one lowercase English character.
+ * It contains at least one uppercase English character.
+ * It contains at least one special character. The special characters are: !@#$%^&*()-+
+ *
+ * Find the minimum number of characters she must add to make her password strong.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING password
+ */
+
+export default function minimumNumber(n, password) {
+
+}
--- test/week5.test.js
+++ test/week5.test.js
@@ -2,10 +2,20 @@ import {
   describe, expect, it,
 } from '@jest/globals';
 import maxMin from '../src/week5/maxMin';
+import minimumNumber from '../src/week5/strongPassword';
 
 describe('maxMin Test', () => {
   it('Must return the minimum possible unfairness', () => {
     const returnedValue = maxMin(4, [1, 2, 3, 4, 10, 20, 30, 40, 100, 200]);
+    const expectedValue = 3;
+
+    expect(returnedValue).toBe(expectedValue);
+  });
+});
+
+describe('strongPassword Test', () => {
+  it('Must return the minimum number of characters to add', () => {
+    const returnedValue = minimumNumber(3, 'Ab1');
     const expectedValue = 3;
 
     expect(returnedValue).toBe(expectedValue);
