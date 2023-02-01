b8e0a0ebaa7b9cc76cf3a6f6da05ffd16c872016
Icaro <luizicaro.s@gmail.com>
2023-02-01 10:57:45 -0300

    Adding missingNumbers Test

---
+++ src/week5/missingNumbers.js
@@ -0,0 +1,13 @@
+/*
+ * Given two arrays of integers.
+ * Find which elements in the second array are missing from the first array.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY arr
+ *  2. INTEGER_ARRAY brr
+ */
+
+export default function missingNumbers(arr, brr) {
+
+}
--- test/week5.test.js
+++ test/week5.test.js
@@ -2,6 +2,7 @@ import {
   describe, expect, it,
 } from '@jest/globals';
 import maxMin from '../src/week5/maxMin';
+import missingNumbers from '../src/week5/missingNumbers';
 import minimumNumber from '../src/week5/strongPassword';
 
 describe('maxMin Test', () => {
@@ -19,5 +20,16 @@ describe('strongPassword Test', () => {
     const expectedValue = 3;
 
     expect(returnedValue).toBe(expectedValue);
+  });
+});
+
+describe('missingNumbers Test', () => {
+  it('Must return an array of integers', () => {
+    const arr = [203, 204, 205, 206, 207, 208, 203, 204, 205, 206];
+    const brr = [203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204];
+    const returnedValue = missingNumbers(arr, brr);
+    const expectedValue = [204, 205, 206];
+
+    expect(returnedValue).toEqual(expectedValue);
   });
 });
