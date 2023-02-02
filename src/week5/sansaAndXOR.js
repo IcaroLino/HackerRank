5f0d304f1739d8245af362eb4abbbb888542c79c
Icaro <luizicaro.s@gmail.com>
2023-02-02 15:07:53 -0300

    Implementing sansaXor function

--- src/week5/sansaAndXOR.js
+++ src/week5/sansaAndXOR.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 /**
  * Sansa has an array. She wants to find the value obtained by XOR-ing the contiguous subarrays,
  * followed by XOR-ing the values thus obtained. Determine this value.
@@ -7,5 +8,26 @@
  */
 
 export default function sansaXor(arr) {
+  if (arr.length % 2 === 0) return 0;
 
+  return arr.reduce((acc, cur, index) => {
+    if (index % 2 !== 0) return acc;
+    return acc ^ cur;
+  }, 0);
 }
+
+/* Alternative Solution (heavy)
+
+  const subArrays = [];
+
+  for (let i = 1; i <= arr.length; i += 1) {
+    for (let index = 0; index <= arr.length; index += 1) {
+      if (index + i > arr.length) break;
+      subArrays.push(arr.slice(index, index + i));
+    }
+  }
+
+  const subArrXOR = subArrays.map((element) => element.reduce((acc, cur) => acc ^ cur, 0));
+  return subArrXOR.reduce((acc, cur) => acc ^ cur, 0);
+
+*/
