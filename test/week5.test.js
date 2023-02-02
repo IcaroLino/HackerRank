a309dcd8c6fa8bc879e95c395c026bb7f1730488
Icaro <luizicaro.s@gmail.com>
2023-02-01 22:47:08 -0300

    Improving test coverage for missingNumbers Test

--- test/week5.test.js
+++ test/week5.test.js
@@ -33,7 +33,7 @@ describe('strongPassword Test', () => {
 
 describe('missingNumbers Test', () => {
   it('Must return an array of integers', () => {
-    const arr = [203, 204, 205, 206, 207, 208, 203, 204, 205, 206];
+    const arr = [203, 204, 205, 206, 207, 208, 203, 204, 205, 206, 200];
     const brr = [203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204];
     const returnedValue = missingNumbers(arr, brr);
     const expectedValue = [204, 205, 206];
