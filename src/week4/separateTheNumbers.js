aa4dfab4edea06724a5f196cb97c3969a9a2c1c1
Icaro <luizicaro.s@gmail.com>
2023-02-03 15:43:14 -0300

    Implementing separateNumbers function

--- src/week4/separateTheNumbers.js
+++ src/week4/separateTheNumbers.js
@@ -5,5 +5,18 @@
  */
 
 export default function separateNumbers(s) {
+  if (s.length > 1) {
+    for (let i = 1; i <= (s.length / 2); i += 1) {
+      const firstElement = s.substring(0, i);
+      let nextElement = BigInt(firstElement) + BigInt(1);
+      let sequence = firstElement + nextElement;
+      while (sequence.length < s.length) {
+        nextElement += BigInt(1);
+        sequence += nextElement;
+      }
+      if (sequence === s) return `YES ${firstElement}`;
+    }
+  }
 
+  return 'NO';
 }
