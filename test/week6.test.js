9973df7334dad3cf5cfde25c681ec20a1f994bfe
Icaro <luizicaro.s@gmail.com>
2023-02-07 18:18:29 -0300

    Adding recursiveDigitalSum Test

--- test/week6.test.js
+++ test/week6.test.js
@@ -2,6 +2,7 @@ import {
   describe, expect, it,
 } from '@jest/globals';
 import gamingArray from '../src/week6/gamingArray1';
+import superDigit from '../src/week6/recursiveDigitSum';
 import balancedSums from '../src/week6/sherlockAndArray';
 
 describe('sherlockAndArray Test', () => {
@@ -31,6 +32,15 @@ describe('gamingArray1 Test', () => {
   it('Must return BOB', () => {
     const returnedValue = gamingArray([3, 1]);
     const expectedValue = 'BOB';
+
+    expect(returnedValue).toBe(expectedValue);
+  });
+});
+
+describe('recursiveDigitSum Test', () => {
+  it('Must return the super digit of n repeated k times', () => {
+    const returnedValue = superDigit(9875, 4);
+    const expectedValue = 8;
 
     expect(returnedValue).toBe(expectedValue);
   });
