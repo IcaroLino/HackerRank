dd96eea7cbed1724fefd522bdf07c2eebc1da885
Icaro <luizicaro.s@gmail.com>
2023-02-20 20:57:19 -0300

    Implementing pylons function

--- src/week7/goodlandElectricity.js
+++ src/week7/goodlandElectricity.js
@@ -17,5 +17,31 @@
  */
 
 export default function pylons(k, arr) {
+  let count = 1;
+  const plantableIndex = [];
+  arr.forEach((element, index) => {
+    if (element === 1) plantableIndex.push(index);
+  });
 
+  let begin = k - 1;
+  while (!plantableIndex.includes(begin)) {
+    begin -= 1;
+    if (begin < 0) return -1;
+  }
+
+  for (let i = plantableIndex.indexOf(begin); i < plantableIndex.length; i += 1) {
+    let powerPlantGap = (2 * k) - 1;
+
+    while (!plantableIndex.includes(plantableIndex[i] + powerPlantGap)) {
+      powerPlantGap -= 1;
+      if (powerPlantGap <= 0) return -1;
+    }
+
+    count += 1;
+    i = plantableIndex.indexOf(plantableIndex[i] + powerPlantGap);
+
+    if ((plantableIndex[i] + k - 1) >= (arr.length - 1)) return count;
+    i -= 1;
+  }
+  return count;
 }
